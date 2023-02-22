e056bbfdb2b8fda5ecbaefbb3dc80210bc6a1e38
Chris C <chris@makecoolstuff.net>
2023-02-22 12:01:56 +1000

    add search box

--- src/NavBar/NavLink.jsx
+++ src/NavBar/NavLink.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import _ from 'lodash'
 
 export default function NavLink(props) {
+  const animateOnHover = (e) => {
+    e.target.querySelector('.nav-icon')
+      .classList.add('nav-icon-animate')
+  }
+  const endAnimation = (e) => {
+    e.target.classList.remove('nav-icon-animate')
+  }
+  const active = props.activePath === props.path
+
   return (
-    <a class={`navbar-item ${props.activePath}`} href={`/${props.path}`}>
-      <img src={props.icon} alt="Set icon" class="nav-icon mr-1-5" width={22} />
+    <a class={`navbar-item ${props.activePath === props.path && 'is-active'}`} href={`/${props.path}`}
+      onClick={() => props.setActivePath(props.path)}
+      onMouseEnter={animateOnHover}
+    >
+      <img src={props.icon} alt="Set icon" class="nav-icon mr-1-5" width={22}
+        onAnimationEnd={endAnimation}
+      />
       {_.capitalize(props.path)}
     </a>
   )
---
+++ src/NavBar/SearchBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SearchBar() {
+  return (
+    <input class="input is-rounded" type="text" placeholder="Search for card" />
+  )
+}
